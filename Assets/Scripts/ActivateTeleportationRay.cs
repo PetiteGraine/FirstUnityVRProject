a907a3280ce58947419d0b5380f7a1f3459fab51
PetiteGraine <remi93300@hotmail.fr>
2024-10-08 15:13:09 +0200

    activate ray when trigger

---
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ActivateTeleportationRay : MonoBehaviour
+{
+    public GameObject rightTeleportation;
+    public InputActionProperty rightActivate;
+
+    void Update()
+    {
+        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1);
+    }
+}
