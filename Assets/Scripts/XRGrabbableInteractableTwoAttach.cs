f6911ce550c32bc0df8717c6791afb2a1cb65881
PetiteGraine <remi93300@hotmail.fr>
2024-10-11 17:11:53 +0200

    minor changes

--- Assets/Scripts/XRGrabbableInteractableTwoAttach.cs
+++ Assets/Scripts/XRGrabbableInteractableTwoAttach.cs
@@ -7,18 +7,14 @@ public class XRGrabbableInteractableTwoAttach : XRGrabInteractable
     public Transform rightAttachTransform;
     public override Transform GetAttachTransform(IXRInteractor interactor)
     {
-        Debug.Log("GetAttachTransform");
-
         Transform i_attachTransform = null;
 
         if (interactor.transform.CompareTag("Left Hand"))
         {
-            Debug.Log("Left");
             i_attachTransform = leftAttachTransform;
         }
         if (interactor.transform.CompareTag("Right Hand"))
         {
-            Debug.Log("Right");
             i_attachTransform = rightAttachTransform;
         }
         return i_attachTransform != null ? i_attachTransform : base.GetAttachTransform(interactor);
